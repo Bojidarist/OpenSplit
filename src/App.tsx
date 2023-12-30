452bc0b7a4e4c1c14e7bd88d949d8609f78cb969
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2023-12-31 00:53:06 +0200

    Add prettier package and format code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.body);
-root.render(<div><h2 className='bg-gray-500 text-center text-white'>Hello from React!</h2></div>);
+root.render(
+  <div>
+    <h2 className="bg-gray-500 text-center text-white">Hello from React!</h2>
+  </div>
+);
