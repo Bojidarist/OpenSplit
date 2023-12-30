12c9245c41953a1a1bce024923ad777cba427aa5
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2023-12-31 01:03:26 +0200

    Update index.html and react root

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-
-const root = createRoot(document.body);
-root.render(
-  <div>
-    <h2 className="bg-gray-500 text-center text-white">Hello from React!</h2>
-  </div>
-);
---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <div>
+      <h2 className="bg-gray-500 text-center text-white">Hello from React!</h2>
+    </div>
+  );
+};
